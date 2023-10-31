e19597fb69e139403163bce9882b3c0ec622f518
Eslam Kamal <eslamehabkamal@gmail.com>
2023-10-31 16:49:04 +0200

    Update speach_text.dart

--- lib/speach_text.dart
+++ lib/speach_text.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart';
-
-class WordsDictionary {
-  String? word;
-  String? originalWord;
-
-  WordsDictionary({this.word, this.originalWord});
-}
+import 'package:lemmatizerx/lemmatizerx.dart';
 
 class SpeachText extends StatefulWidget {
   const SpeachText({Key? key}) : super(key: key);
@@ -17,26 +11,24 @@ class SpeachText extends StatefulWidget {
 }
 
 class _SpeachTextState extends State<SpeachText> {
-  // List<WordsDictionary> wordsDictionary = [
-  //   WordsDictionary(word: "Kitchen", originalWord: "Kitchen"),
-  //   WordsDictionary(word: "cooking area", originalWord: "Kitchen"),
-  // ];
-  Map<String, String> wordsDictionaryMap = {
-    "kitchen": "kitchen",
-    "cooking area": "kitchen",
-    "kitchenette": "kitchen",
-    "cookery": "kitchen",
-    "bathroom": "bathroom",
-    "restroom": "bathroom",
-    "toilet": "bathroom",
-    "washroom": "bathroom",
-    "meeting room": "meeting room",
-    "boardroom": "meeting room",
-    "assembly room": "meeting room",
-    "hall": "meeting room",
-    "hr room ": "hr room ",
-    "hr": "hr room "
-  };
+  List<String> wordsList = [
+    "kitchen",
+    "cooking area",
+    "kitchenette",
+    "cookery",
+    "bathroom",
+    "restroom",
+    "toilet",
+    "washroom",
+    "meeting room",
+    "boardroom",
+    "assembly room",
+    "hall",
+    "hr room ",
+    "hr"
+  ];
+
+  Map<String, String> synonymsMap = {}; // New map to store synonyms
 
   List<String> words = [];
 
@@ -44,40 +36,45 @@ class _SpeachTextState extends State<SpeachText> {
   bool _speechEnabled = false;
   String _lastWords = '';
 
+  Lemmatizer lemmatizer = Lemmatizer();
+
   @override
   void initState() {
     super.initState();
     _initSpeech();
+    _generateSynonyms();
   }
 
-  /// This has to happen only once per app
+  void _generateSynonyms() {
+    for (final word in wordsList) {
+      final lemma = lemmatizer.lemma(word, POS.NOUN);
+      final synonyms = lemma.lemmas.where((lemma) => lemma != word).toList();
+      for (final synonym in synonyms) {
+        synonymsMap[synonym] = word;
+      }
+    }
+  }
+
   void _initSpeech() async {
     _speechEnabled = await _speechToText.initialize();
     setState(() {});
   }
 
-  /// Each time to start a speech recognition session
   void _startListening() async {
     await _speechToText.listen(onResult: _onSpeechResult);
     setState(() {});
   }
 
-  /// Manually stop the active speech recognition session
-  /// Note that there are also timeouts that each platform enforces
-  /// and the SpeechToText plugin supports setting timeouts on the
-  /// listen method.
   void _stopListening() async {
     await _speechToText.stop();
     setState(() {});
   }
 
-  /// This is the callback that the SpeechToText plugin calls when
-  /// the platform returns recognized words.
   void _onSpeechResult(SpeechRecognitionResult result) {
     setState(() {
       _lastWords = result.recognizedWords;
       words = _lastWords.split(' ');
-      words.removeWhere((item) => !wordsDictionaryMap.keys.contains(item));
+      words.removeWhere((item) => !synonymsMap.keys.contains(item));
     });
   }
 
@@ -102,13 +99,8 @@ class _SpeachTextState extends State<SpeachText> {
               child: Container(
                 padding: EdgeInsets.all(16),
                 child: Text(
-                  // If listening is active show the recognized words
                   _speechToText.isListening
                       ? '$_lastWords'
-                      // If listening isn't active but could be tell the user
-                      // how to start it, otherwise indicate that speech
-                      // recognition is not yet ready or not supported on
-                      // the target device
                       : _speechEnabled
                           ? 'Tap the microphone to start listening...'
                           : 'Speech not available',
@@ -125,7 +117,6 @@ class _SpeachTextState extends State<SpeachText> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed:
-            // If not yet listening for speech start, otherwise stop
             _speechToText.isNotListening ? _startListening : _stopListening,
         tooltip: 'Listen',
         child: Icon(_speechToText.isNotListening ? Icons.mic_off : Icons.mic),
